c1d12530ddc904a1ab0c34123d4f50f3cb0e6d8d
Md Shahriar Zaman <mzaman30@myseneca.ca>
2025-07-18 09:51:43 -0400

    Completed Lab 7

--- lab7f.py
+++ lab7f.py
@@ -2,34 +2,33 @@
 # Student ID: mzaman30
 
 class Time:
-    """Simple object type for time of the day.
-       data attributes: hour, minute, second
-       function attributes: __init__, __str__, __repr__,
-                            time_to_sec, format_time,
-                            change_time, sum_times
-    """
     def __init__(self,hour=12,minute=0,second=0):
         self.hour = hour
         self.minute = minute
         self.second = second
+
+    def __str__(self):
+        return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
+
+    def __repr__(self):
+        return f'{self.hour:02d}.{self.minute:02d}.{self.second:02d}'
 
     def format_time(self):
         return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
 
     def sum_times(self, t2):
         total_seconds = self.time_to_sec() + t2.time_to_sec()
-        nt = sec_to_time(total_seconds)
-        return nt
+        return sec_to_time(total_seconds)
+
+    def __add__(self, t2):
+        return self.sum_times(t2)
 
     def change_time(self, seconds):
-        time_seconds = self.time_to_sec()
-        nt = sec_to_time(time_seconds + seconds)
+        nt = sec_to_time(self.time_to_sec() + seconds)
         self.hour, self.minute, self.second = nt.hour, nt.minute, nt.second
 
     def time_to_sec(self):
-        minutes = self.hour * 60 + self.minute
-        seconds = minutes * 60 + self.second
-        return seconds
+        return (self.hour * 60 + self.minute) * 60 + self.second
 
     def valid_time(self):
         if self.hour < 0 or self.minute < 0 or self.second < 0:
